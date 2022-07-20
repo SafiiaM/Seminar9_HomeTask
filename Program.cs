301e5f9a8cdcf920ab26edb15dba2b41b6568d67
SafiiaM <107404606+SafiiaM@users.noreply.github.com>
2022-07-20 17:37:49 +0300

    add Task66

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
-// Выполнить с помощью рекурсии.
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в 
+// промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
 Console.Clear();
 
-int n = InputNumbers($"Задайте значение N: ");
-int count = 2;
-PrintNumber(n, count);
-Console.Write(1);
+int m = InputNumbers("Задайте значение М: ");
+int n = InputNumbers("Задайте значение N: ");
+int a = m;
 
-void PrintNumber(int n, int count)
+if (m > n) 
 {
-  if (count > n) return;
-  PrintNumber(n, count + 1);
-  Console.Write(count + ", ");
+  m = n; 
+  n = a;
+}
+
+PrintSum(m, n, a=0);
+
+void PrintSum(int m, int n, int sum)
+{
+  sum = sum + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {sum} ");
+    return;
+  }
+  PrintSum(m, n - 1, sum);
 }
 
 int InputNumbers(string input) 
@@ -23,4 +34,3 @@ int InputNumbers(string input)
   int output = Convert.ToInt32(Console.ReadLine());
   return output;
 }
-
